40b85ac0a9173cbd3d4bfbdaa5fca66cc0eb42ad
Jonathan <colombojonathan@virgilio.it>
2021-06-08 17:54:29 +0200

    Add files via upload

--- Esercizio_1/Esercizio_1/Program.cs
+++ Esercizio_1/Esercizio_1/Program.cs
@@ -10,7 +10,8 @@ namespace Esercizio_1
     {
         static void Main(string[] args)
         {
-            // commento
+            // commento 
+
             DateTime today = DateTime.Now;
             Console.WriteLine("Data:{0}", today);
             Console.WriteLine("Giorno:{0:D}", today);
@@ -35,77 +36,79 @@ namespace Esercizio_1
                 }
 
             } while (key != ConsoleKey.Enter);
-            
-            if(password == "ciao")
+
+            if (password == "ciao")
             {
                 char choosen;
-                int number1 = 0, number2 = 0;
-                Console.Clear();
-                // Menù
-                Console.WriteLine("Benvenuto nella calcolatrice");
-                Console.WriteLine("1. Addizione");
-                Console.WriteLine("2. Sottrazione");
-                Console.WriteLine("3. Moltiplicazione");
-                Console.WriteLine("4. Divisione");
-                Console.WriteLine("5. Esci");
-                choosen = Console.ReadKey().KeyChar;
-
-                if(choosen != '5')
+                do
                 {
-                    Console.WriteLine("Inserisci il primo numero");
-                    number1 = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Inserisci il secondo numero");
-                    number2 = Convert.ToInt32(Console.ReadLine());
+                    int number1 = 0, number2 = 0;
+                    Console.Clear();
+                    // Menù
+                    Console.WriteLine("Benvenuto nella calcolatrice");
+                    Console.WriteLine("1. Addizione");
+                    Console.WriteLine("2. Sottrazione");
+                    Console.WriteLine("3. Moltiplicazione");
+                    Console.WriteLine("4. Divisione");
+                    Console.WriteLine("5. Esci");
+                    choosen = Console.ReadKey().KeyChar;
+                    Console.WriteLine("");
 
-                }
+                    if (choosen != '5')
+                    {
+                        Console.Write("Inserisci il primo numero: ");
+                        number1 = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Inserisci il secondo numero: ");
+                        number2 = Convert.ToInt32(Console.ReadLine());
+                    }
 
-                //calcolo
-                int result;
-                switch(choosen)
-                {
-                    case '1': // Somma
-                        {
-                            result = number1 + number2;
-                            Console.WriteLine(number1 + " + " + number2 + " = " + result);
-                            break;
-                        }
-
-                    case '2': // Sottrazione
-                        {
-                            result = number1 - number2;
-                            Console.WriteLine(number1 + " - " + number2 + " = " + result);
-                            break;
-                        }
-
-                    case '3': // Moltiplicazione
-                        {
-                            result = number1 * number2;
-                            Console.WriteLine(number1 + " * " + number2 + " = " + result);
-                            break;
-                        }
-
-                    case '4': // Divisione
-                        {
-                            if(number2 != 0)
+                    //calcolo
+                    int result;
+                    switch (choosen)
+                    {
+                        case '1': // Somma
                             {
-                                result = number1 / number2;
-                                Console.WriteLine(number1 + " / " + number2 + " = " + result);
+                                result = number1 + number2;
+                                Console.WriteLine(number1.ToString() + " + " + number2.ToString() + " = " + result);
                                 break;
                             }
+
+                        case '2': // Sottrazione
+                            {
+                                result = number1 - number2;
+                                Console.WriteLine(number1.ToString() + " - " + number2.ToString() + " = " + result);
+                                break;
+                            }
+
+                        case '3': // Moltiplicazione
+                            {
+                                result = number1 * number2;
+                                Console.WriteLine(number1.ToString() + " * " + number2.ToString() + " = " + result);
+                                break;
+                            }
+
+                        case '4': // Divisione
+                            {
+                                if (number2 != 0)
+                                {
+                                    result = number1 / number2;
+                                    Console.WriteLine(number1.ToString() + " / " + number2.ToString() + " = " + result);
+                                }
+                                break;
+                            }
+
+                        case '5': // Uscita
                             break;
-                        }
 
-                    case '5': // Uscita
-                        break;
+                        default: // scelta non programmata
+                            {
+                                Console.WriteLine("Opzione errata");
+                                break;
+                            }
+                    } Console.ReadLine();
+                } while (choosen != '5');
 
-                    default:
-                        {
-                            Console.WriteLine();
-                            break;
-                        }
-
-                }
-            }
+            } 
             else
             {
                 Console.WriteLine("Password Errata");
