71b02052a99b8b57205051699e2a55d8702386f0
JonathanColo98 <colombojonathan@virgilio.it>
2021-06-10 17:06:38 +0200

    Exercise 11

---
+++ Esercizio_11/Esercizio_11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Esercizio_11
+{
+    [Serializable()]
+
+    public class Car
+    {
+        [System.Xml.Serialization.XmlElement("StockNumber")]
+
+        public string StockNumber { get; set; }
+        [System.Xml.Serialization.XmlElement("Make")]
+
+        public string Make { get; set; }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
