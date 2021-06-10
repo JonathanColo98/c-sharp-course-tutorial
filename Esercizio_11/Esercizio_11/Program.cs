ea7afac0110f352063671495525ac2cc43592eb6
JonathanColo98 <colombojonathan@virgilio.it>
2021-06-10 17:30:52 +0200

    Exercise 11 done

--- Esercizio_11/Esercizio_11/Program.cs
+++ Esercizio_11/Esercizio_11/Program.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Serialization;
 
+// Creare un file di xml di prova e specificare la PATH per testarlo
+
 namespace Esercizio_11
 {
     [Serializable()]
-
     public class Car
     {
         [System.Xml.Serialization.XmlElement("StockNumber")]
@@ -19,13 +21,44 @@ namespace Esercizio_11
 
         public string Make { get; set; }
 
+        public string Model { get; set; }
     }
 
-
+    [Serializable()]
+    [System.Xml.Serialization.XmlRoot("CarCollection")]
+    public class CarCollection
+    {
+        [XmlArray("Cars")]
+        [XmlArrayItem("Car", typeof(Car))]
+        public Car[] Car { get; set; }
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
+            var cars = new CarCollection();
+            cars = XCar();
+
+            //vedere a video il file
+            foreach (var car in cars.Car)
+            {
+                Console.WriteLine(car.Make + " - " + car.Model + " - " + car.StockNumber);
+            }
+
+            cars = null;
+            Console.ReadLine();
+        }
+
+        static CarCollection XCar()
+        {
+            // Deserializzazione
+            // inserire in StreamReader, il percorso del file xml
+            using (TextReader reader = new StreamReader(@""))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(CarCollection));
+                return (CarCollection)serializer.Deserialize(reader);
+            }
         }
     }
 }
