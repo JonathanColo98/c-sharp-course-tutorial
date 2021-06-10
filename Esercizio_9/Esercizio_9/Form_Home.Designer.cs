239ce1b4caebc1813f9036e1d019c22256e97ee3
JonathanColo98 <colombojonathan@virgilio.it>
2021-06-10 15:38:16 +0200

    Exercise 9 done

---
+++ Esercizio_9/Esercizio_9/Form_Home.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace Esercizio_9
+{
+    partial class Form_Home
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Title = new System.Windows.Forms.Label();
+            this.txt_Read = new System.Windows.Forms.TextBox();
+            this.btn_Read = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_Title
+            // 
+            this.lbl_Title.AllowDrop = true;
+            this.lbl_Title.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Title.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lbl_Title.Location = new System.Drawing.Point(12, 9);
+            this.lbl_Title.Name = "lbl_Title";
+            this.lbl_Title.Size = new System.Drawing.Size(776, 23);
+            this.lbl_Title.TabIndex = 0;
+            this.lbl_Title.Text = "TEST";
+            this.lbl_Title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txt_Read
+            // 
+            this.txt_Read.Location = new System.Drawing.Point(12, 77);
+            this.txt_Read.Multiline = true;
+            this.txt_Read.Name = "txt_Read";
+            this.txt_Read.Size = new System.Drawing.Size(776, 220);
+            this.txt_Read.TabIndex = 1;
+            // 
+            // btn_Read
+            // 
+            this.btn_Read.Location = new System.Drawing.Point(353, 325);
+            this.btn_Read.Name = "btn_Read";
+            this.btn_Read.Size = new System.Drawing.Size(108, 37);
+            this.btn_Read.TabIndex = 2;
+            this.btn_Read.Text = "Leggi LOG";
+            this.btn_Read.UseVisualStyleBackColor = true;
+            this.btn_Read.Click += new System.EventHandler(this.btn_Read_Click);
+            // 
+            // Form_Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Read);
+            this.Controls.Add(this.txt_Read);
+            this.Controls.Add(this.lbl_Title);
+            this.Name = "Form_Home";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "First App";
+            this.Load += new System.EventHandler(this.Form_Home_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Title;
+        private System.Windows.Forms.TextBox txt_Read;
+        private System.Windows.Forms.Button btn_Read;
+    }
+}
+
---
+++ Esercizio_9/Esercizio_9/Form_Home.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Esercizio_9
+{
+    public partial class Form_Home : Form
+    {
+        //per indiciare un percorso inserire la @ all'inizio della String
+        String Path = @"C:\workspaceC-Sharp\c-sharp-course-tutorial\TEMP\Accessi.txt";
+
+        public Form_Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Home_Load(object sender, EventArgs e)
+        {
+            using (FileStream FS = new FileStream(Path, FileMode.Append, FileAccess.Write))
+            
+            using (StreamWriter SW = new StreamWriter(FS))
+            {
+                SW.WriteLine("Accesso del: " + DateTime.Now);
+            }
+
+        }
+
+        private void btn_Read_Click(object sender, EventArgs e)
+        {
+            int Count = 0;
+
+            using (StreamReader SR = new StreamReader(Path))
+            {
+                string Line = "";
+                while ((Line = SR.ReadLine()) != null)
+                {
+                    if (Count != 0)
+                    {
+                        // ci sono più righe 
+                        txt_Read.Text = txt_Read.Text + "\r\n" + Line;
+                    } 
+                    else
+                    {
+                        // c'è una riga sola
+                        txt_Read.Text = Line;
+                    }
+                    Count++;
+                }
+
+            }
+
+
+
+        }
+    }
+}
